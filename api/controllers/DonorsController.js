005e24fe1f973d8416637b5326f4fd1dd0d27279
donmostro <rodrigo.riquelme.e@gmail.com>
2016-05-10 22:16:49 -0300

    mapa

---
+++ api/controllers/DonorsController.js
@@ -0,0 +1,11 @@
+/**
+ * DonorsController
+ *
+ * @description :: Server-side logic for managing Donors
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
---
+++ api/models/Donors.js
@@ -0,0 +1,54 @@
+/**
+ * Donors.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    first_name: {
+      type: 'string',
+      required: true
+    },
+    last_name: {
+      type: 'string',
+      required: true
+    },
+    contact_number: {
+      type: 'integer',
+      required: true
+    },
+    email: {
+      type: 'string',
+      required: true
+    },
+    address: {
+      type: 'string',
+      required: true
+    },
+    blood_group: {
+      type: 'string',
+      enum : ['O-', 'O+', 'B-', 'B+', 'A-', 'A+', 'AB-', 'AB+'],
+      required: true
+    },
+    ip: {
+      type: 'string',
+      required: true
+    },
+    address: {
+      type: 'string',
+      required: true
+    },
+    coord_x: {
+      type: 'float',
+      required: true
+    },
+    coord_y: {
+      type: 'float',
+      required: true
+    }
+  }
+};
+
