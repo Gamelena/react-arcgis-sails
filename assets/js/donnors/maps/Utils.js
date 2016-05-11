9bd547a0558172e0621078545e2a3fe4081f2836
donmostro <rodrigo.riquelme.e@gmail.com>
2016-05-11 11:52:46 -0300

    switch location

--- assets/js/donnors/maps/Utils.js
+++ assets/js/donnors/maps/Utils.js
@@ -1,62 +1,22 @@
-var self;
-define(["dojo/_base/declare", "esri/geometry/Point", "esri/graphic",
+define(["dojo/_base/declare", "esri/geometry/Point", "esri/graphic", "esri/geometry/Point",
     "esri/symbols/SimpleMarkerSymbol", "esri/symbols/SimpleLineSymbol", "esri/Color"
-], function (declare, Point, Graphic,
+], function (declare, Point, Graphic, Point,
     SimpleMarkerSymbol, SimpleLineSymbol, Color) {
+
     return declare("donnors.maps.Utils", null, {
         map: null,
 
         graphic: null,
 
+        zoom : 15,
+
+        watchId : null,
+
         constructor: function (args) {
             dojo.mixin(this, args);
         },
 
-        checkLocation: function () {
-            self = this;
-            if (navigator.geolocation) {
-                navigator.geolocation.getCurrentPosition(self.zoomToLocation, self.locationError);
-                watchId = navigator.geolocation.watchPosition(self.showLocation, self.locationError);
-            } else {
-                console.error("Geolocation Not Supported");
-            }
-        },
-
-        locationError: function (error) {
-            switch (error.code) {
-                case error.PERMISSION_DENIED:
-                    console.error("Location not provided");
-                    break;
-                case error.POSITION_UNAVAILABLE:
-                    console.error("Current location not available");
-                    break;
-                case error.TIMEOUT:
-                    console.error("Timeout");
-                    break;
-                default:
-                    alert("unknown error");
-                    break;
-            }
-        },
-
-        zoomToLocation: function (location) {
-            var pt = new Point(location.coords.longitude, location.coords.latitude);
-            self.addGraphic(pt);
-            self.map.centerAndZoom(pt, 15);
-        },
-
-        showLocation: function (location) {
-            //zoom to the users location and add a graphic
-            var pt = new Point(location.coords.longitude, location.coords.latitude);
-            if (!graphic) {
-                self.addGraphic(pt);
-            } else { // move the graphic if it already exists
-                self.graphic.setGeometry(pt);
-            }
-            self.map.centerAt(pt);
-        },
-
-        addGraphic: function (pt) {
+        addGraphic: function (point) {
             var symbol = new SimpleMarkerSymbol(
                 SimpleMarkerSymbol.STYLE_CIRCLE,
                 12,
@@ -67,8 +27,49 @@ define(["dojo/_base/declare", "esri/geometry/Point", "esri/graphic",
                 ),
                 new Color([210, 105, 30, 0.9])
             );
-            self.graphic = new Graphic(pt, symbol);
-            self.map.graphics.add(graphic);
+            this.graphic = new Graphic(point, symbol);
+            this.map.graphics.add(this.graphic);
+        },
+
+        locationError: function(error){
+            if (navigator.geolocation) {
+                navigator.geolocation.clearWatch(this.watchId);
+            }
+
+            switch (error.code){
+                case error.PERMISSION_DENIED: console.error("Location not provided");
+                    break;
+                case error.POSITION_UNAVAILABLE:  console.error("Current location not available");
+                    break;
+                case error.TIMEOUT:  console.error("Timeout");
+                    break;
+                default: alert("unknown error");
+                    break;
+            }
+        },
+
+        zoomToLocation: function(longitude, latitude) {
+            var point = new Point(longitude, latitude);
+            this.addGraphic(point);
+            this.map.centerAndZoom(point, this.zoom);
+        },
+
+        showLocation: function (longitude, latitude) {
+            //zoom to the users location and add a graphic
+            var point = new Point(longitude, latitude);
+            if ( !this.graphic ) {
+                this.addGraphic(point);
+            } else { // move the graphic if it already exists
+                this.graphic.setGeometry(point);
+            }
+            this.map.centerAt(point);
+        },
+
+        updateLocation: function(longitude, latitude) {
+            this.showLocation(longitude, latitude);
+            var point = new Point(longitude, latitude);
+            this.map.centerAndZoom(point, this.zoom);
+
         }
     });
 });
